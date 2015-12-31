9f324f13f8f6e63d95ea2d4f1b16386aa5c93194
Brett McLarnon <brett.mclarnon@gmail.com>
2015-12-31 14:19:56 -0800

    Fix email subject and body to include "wedding".

--- save-the-date/send_mail.py
+++ save-the-date/send_mail.py
@@ -12,7 +12,7 @@ from apiclient import discovery
 from oauth2client import client
 
 
-_SUBJECT = 'Save the Date: Aug 20, 2016'
+_SUBJECT = 'Save the Date for Jen & Brett\'s Wedding'
 
 
 def main():
